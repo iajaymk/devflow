7ca2d876d9ee11603f2956d8530fda15220a0959
Ajay Kumar <ajaykumarmkaju@gmail.com>
2026-02-17 21:25:20 +0530

    Fix: Typo - dark classname

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none">
       <Link href="/" className="flex items-center gap-1">
         <Image src="/images/site-logo.svg" width={23} height={23} alt="DevOverFlow logo" />
-        <p className="h2-bold font-space-grotesk text-dark-100 darkk:text-light-900 max-sm:hidden">
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev <span className="text-primary-500">OverFlow</span>
         </p>
       </Link>
